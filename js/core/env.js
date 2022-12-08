11f7f81b206ae55b92585ffb074c38c0420f1d29
darwin mamani <darwin_xd10@hotmail.com>
2022-12-08 18:00:07 -0400

    darwin finish

---
+++ js/core/env.js
@@ -0,0 +1,6 @@
+const variables = { 
+    socket : 'http://localhost:9090',
+    app : 'http://localhost/sw1-p2-modelo-c4/public'
+}
+
+module.exports =  variables;
--- js/core/watch.js
+++ js/core/watch.js
@@ -1,6 +1,8 @@
 'use strict'
+const variables = require('./env');
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:9090');
+// const socket = io.connect('http://localhost:9090');
+const socket = io.connect(variables.socket);
 console.log('socket cargado...');
 
 module.exports = {
